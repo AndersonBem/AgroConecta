8c3f43dedab1c844e85759b847529ee9cc72d421
AndersonBem <126602982+AndersonBem@users.noreply.github.com>
2025-11-19 13:37:44 -0300

    Atualização templates pythonicos, botão editar cooperativa funcional, view de criação utilizando o mesmo template de editar, procedure para editar cooperativa

--- apps/index/urls.py
+++ apps/index/urls.py
@@ -1,4 +1,4 @@
-from apps.index.views import index, login_view, cadastro_cooperativa, cadastro_gestor, gestao_cooperativa,home
+from apps.index.views import index, login_view, cadastro_cooperativa, cadastro_gestor, gestao_cooperativas,home, editar_cooperativa
 from django.urls import path
 
 
@@ -6,8 +6,9 @@ urlpatterns = [
         path('', index, name='index'),
         path('login_view/', login_view, name='login_view'),
         path('cadastro_cooperativa/', cadastro_cooperativa, name='cadastro_cooperativa'),
+        path("cooperativas/<path:cnpj>/editar/", editar_cooperativa, name="editar_cooperativa"),
         path('cadastro_gestor/', cadastro_gestor, name='cadastro_gestor'),
-        path('gestao_cooperativa/', gestao_cooperativa, name='gestao_cooperativa'),
-         path('home/', home, name='home')
+        path('gestao_cooperativa/', gestao_cooperativas, name='gestao_cooperativa'),
+        path('home/', home, name='home')
         
     ]   
